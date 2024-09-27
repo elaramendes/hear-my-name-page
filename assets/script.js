11cdfca73a5a05cbe969c51aef1b3ae9cd71e344
Elara Mendes <182012664+elaramendes@users.noreply.github.com>
2024-09-27 19:48:09 -0300

    Made the page responsive

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function showItems() {
+    let nav_items = document.querySelector('.nav-links');
+
+    nav_items.classList.toggle('show');
+
+    //if (nav_items.classList.contains === 'show') {
+        //nav_items.classList.remove('show');
+    //} else {
+       // nav_items.classList.add('show');
+    //}
+};
